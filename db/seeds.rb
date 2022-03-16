270f9c3bebd7334201e88ffacf0cb89281caea9d
Azim Murad <azim.murad17@gmail.com>
2022-03-16 22:46:26 +1100

    Adding User model and running migrations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+User.create(username: "Azim", password: "Azim123")
+User.create(username: "Lubna", password: "Lubna123")
+User.create(username: "Misbah", password: "Misbah123")
+User.create(username: "Aroosa", password: "Aroosa123")
+User.create(username: "Sahir", password: "Sahir123")
