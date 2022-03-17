c69e09f0c884ac2e120d7fad7c9168f214066dc6
Azim Murad <azim.murad17@gmail.com>
2022-03-17 15:01:11 +1100

    Adding restrictions at controller layer

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -1,4 +1,9 @@
 class ChatroomController < ApplicationController
+    before_action :require_user
+    #Helper method from Application controller
+
+
+
     def index 
     	@messages = Message.all
     end
