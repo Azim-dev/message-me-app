4e316051fcc59aabcc1d6fc17de2b66fb64a3304
Azim Murad <azim.murad17@gmail.com>
2022-03-20 00:08:01 +1100

    troubleshooting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,5 +91,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_cable.allowed_request_origins = ['https://vast-oasis-78865.herokuapp.com/']
+  config.action_cable.allowed_request_origins = ['https://vast-oasis-78865.herokuapp.com']
 end
